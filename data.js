fa82e0f7e048dcc99196421b95ddb21216186334
murier <cindy.murier@epfl.ch>
2023-03-24 11:00:07 +0100

    Update database

---
+++ data.js
@@ -0,0 +1,68 @@
+export const arts = {
+	bernmuseum: [
+		{
+			artiste: "",
+			titre: "",
+			date: "",
+			dimensions: {
+				hauteur: "",
+				longueur: "",
+				profondeur: "",
+			},
+			objet: "",
+			anneeacquisition: "",
+			provenance: {
+				nom: "",
+				prenom: "",
+				pays: "",
+				annee: "",
+			},
+			numeroinventaire: "",
+			emplacement: "",
+		},
+	],
+	baselmuseum: [
+		{
+			artiste: "",
+			titre: "",
+			date: "",
+			dimensions: {
+				hauteur: "",
+				longueur: "",
+				profondeur: "",
+			},
+			objet: "",
+			anneeacquisition: "",
+			provenance: {
+				nom: "",
+				prenom: "",
+				pays: "",
+				annee: "",
+			},
+			numeroinventaire: "",
+			emplacement: "",
+		},
+	],
+	lausannemuseum: [
+		{
+			artiste: "",
+			titre: "",
+			date: "",
+			dimensions: {
+				hauteur: "",
+				longueur: "",
+				profondeur: "",
+			},
+			objet: "",
+			anneeacquisition: "",
+			provenance: {
+				nom: "",
+				prenom: "",
+				pays: "",
+				annee: "",
+			},
+			numeroinventaire: "",
+			emplacement: "",
+		},
+	],
+};
